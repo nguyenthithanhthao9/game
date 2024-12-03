767b47dbb7a69cf2071ee2256d379f85716c4bc5
nguyenthithanhthao9 <thaontt.24ce@vku.udn.vn>
2024-12-03 23:11:40 +0700

    hi

---
+++ Peashooter.java
@@ -0,0 +1,31 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * Created by Armin on 6/25/2016.
+ */
+public class Peashooter extends Plant {
+
+    public Timer shootTimer;
+
+
+    public Peashooter(GamePanel parent, int x, int y) {
+        super(parent, x, y);
+        shootTimer = new Timer(2000, (ActionEvent e) -> {
+            //System.out.println("SHOOT");
+            if (getGp().getLaneZombies().get(y).size() > 0) {
+                getGp().getLanePeas().get(y).add(new Pea(getGp(), y, 103 + this.getX() * 100));
+            }
+        });
+        shootTimer.start();
+    }
+
+    @Override
+    public void stop() {
+        shootTimer.stop();
+    }
+    Image img = Toolkit.getDefaultToolkit().getImage( "C:\\Users\\ASUS\\OneDrive\\Documents\\Downloads\\Peashooter.png");
+
+}
---
+++ Sunflower.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * Created by Armin on 6/28/2016.
+ */
+public class Sunflower extends Plant {
+
+    private Timer sunProduceTimer;
+
+    public Sunflower(GamePanel parent, int x, int y) {
+        super(parent, x, y);
+        sunProduceTimer = new Timer(15000, (ActionEvent e) -> {
+            Sun sta = new Sun(getGp(), 60 + x * 100, 110 + y * 120, 130 + y * 120);
+            getGp().getActiveSuns().add(sta);
+            getGp().add(sta, new Integer(1));
+        });
+        sunProduceTimer.start();
+    }
+    Image img = Toolkit.getDefaultToolkit().getImage( "C:\\Users\\ASUS\\OneDrive\\Documents\\Downloads\\sunflower.png");
+
+}
